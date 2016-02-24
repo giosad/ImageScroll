899849e4b9e15b2af1f2efc581ac8d54c324cab6
Gennadi Iosad <gena@lightricks.com>
2016-02-24 12:19:21 +0200

    new project

---
+++ ImageScroll/ImageScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageScroll
+//
+//  Created by Gennadi Iosad on 24/02/2016.
+//  Copyright © 2016 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageScroll/ImageScroll/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageScroll
+//
+//  Created by Gennadi Iosad on 24/02/2016.
+//  Copyright © 2016 n/a. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
